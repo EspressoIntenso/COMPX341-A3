8388e789c51c64929770421b593928a186161c22
EspressoIntenso <maxbracken1@gmail.com>
2022-05-20 02:52:19 +1200

    COMPX341-22A-A3 Testing Commit Message

--- assets/pipeline.ps1
+++ assets/pipeline.ps1
@@ -1,13 +1,16 @@
+if (!$args.count -eq 1){
+    Write-Host "Usage: pipeline.ps1 commitMessage"
+    exit
+}
 npm install
 npm run build
-if ($LASTEXITCODE)
-{
+if ($LASTEXITCODE){
     #Exit code was 1 an error has occured 
     Write-Host "An error has occured, exiting and not commiting"
     exit
 }
 #Has compiled successfully, commit to git
 Write-Host "No error has occured, pushing changes to repo"
-git commit -a -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+git commit -a -m $args[0]
 git push origin main
 npm run start
